b2770aae13ddd5808cd186f069a1e306907d3600
Usanas7 <156175184+Usanas7@users.noreply.github.com>
2025-04-03 15:33:56 +0200

    Updated NGO logos

--- Utils/NgoData.js
+++ Utils/NgoData.js
@@ -1,77 +1,33 @@
 
 const SeedData = [
   {
-    name: "Ministry of Labour and Employment",
+    name: "African Forum for Agricultural Advisory Services",
     image:
-      "https://res.cloudinary.com/dw2dqblhs/image/upload/v1684039827/Janseva/labour_rh0mh7.jpg",
+      "https://upload.wikimedia.org/wikipedia/en/thumb/2/2f/AFAAS_logo_new.png/220px-AFAAS_logo_new.png",
     description:
-      "The Ministry of Labour and Employment is responsible for formulating and implementing policies and programs related to employment, job creation, and workers' welfare in India. It oversees several agencies that provide employment services and support.",
+      "The African Forum for Agricultural Advisory Services, is an African organization for strengthening Agricultural Extension and Advisory Services in Africa.",
     category: "Government",
-    link: "https://labour.gov.in/",
+    link: "https://www.afaas-africa.org/rwanda/",
   },
   {
-    name: "National Career Service (NCS)",
+    name: "Ministry of Agriculture and Animal Resources",
     image:
-      "https://res.cloudinary.com/dw2dqblhs/image/upload/v1684039857/Janseva/l7OUmrFU_QD8taT_ICxg-mlMmf-IjXt5UqJ4pjrHxFkB9IWFN6rbtcgE7W7Ds0Zctg_x9goc9.png",
+      "https://www.minagri.gov.rw/index.php?eID=dumpFile&t=f&f=1679&token=c456432515e20118795fbbd0cce379ac2bcd0a14",
     description:
-      " NCS is an online platform developed by the Ministry of Labour and Employment to provide job-related services. It offers job listings, career counseling, skill assessments, and information about government schemes and programs.",
+      " The Ministry of Agriculture and Animal Resources is a ministry of Rwanda with headquarters at Kigali, Rwanda. It is headed currently by Ildephonse Musafiri since 2023. It was established by the Government of Rwanda to aid promote agriculture and livestock sector of the country.",
     category: "Government",
-    link: "https://www.ncs.gov.in/Pages/default.aspx",
+    link: "https://www.minagri.gov.rw/",
   },
   {
-    name: "National Rural Employment Guarantee Act (NREGA)",
+    name: "Cultivating New Frontiers in Agriculture",
     image:
-      "https://res.cloudinary.com/dw2dqblhs/image/upload/v1684039908/Janseva/mgnartega_r71lmj.jpg",
+      "https://theclimateregistry.org/wp-content/uploads/2022/12/CNFA_LogoTag_Lockup_RGB-transparent.png",
     description:
-      "NREGA aims to provide employment opportunities and ensure livelihood security for rural workers. It guarantees a certain number of days of wage employment to eligible households in rural areas.",
+      "Cultivating New Frontiers in Agriculture is a non-profit international development organization based in Washington, DC. CNFA's mission is to increase and sustain rural incomes in less developed areas of the world by assisting farmers and rural entrepreneurs",
     category: "Government",
-    link: "https://nrega.nic.in/netnrega/mgnrega_new/NREGA_home.aspx",
+    link: "https://cnfa.org/",
   },
-  {
-    name: "National Skill Development Corporation (NSDC)",
-    image:
-      "https://res.cloudinary.com/dw2dqblhs/image/upload/v1684040013/Janseva/NSDC_Logo_in_clear_background_zhciqh.png",
-    description:
-      "NSDC is a public-private partnership organization that promotes skill development and vocational training in India. It collaborates with various training providers and industries to enhance the employability of workers.",
-    category: "Government",
-    link: "https://nsdcindia.org/",
-  },
-  {
-    name: "Self-Employed Women's Association (SEWA)",
-    image:
-      "https://res.cloudinary.com/dw2dqblhs/image/upload/v1684300560/Janseva/e7d3600d-bf6e-4fbb-971b-75f44cb19525_r4adcq.jpg",
-    description:
-      "SEWA is a prominent NGO in India that focuses on the empowerment of women working in the informal sector. It provides support to small-scale workers, including laborers, electricians, plumbers, and more. SEWA offers various services such as skill training, financial assistance, access to social security, healthcare, and advocacy for workers' rights. Their aim is to enhance the livelihoods and social status of these workers.",
-    category: "NGO",
-    link: "https://www.sewa.org/",
-  },
-  {
-    name: "Prayas",
-    image:
-      "https://res.cloudinary.com/dw2dqblhs/image/upload/v1684300629/Janseva/5qho1rXK_400x400_nhtxtb.jpg",
-    description:
-      "Prayas is an NGO operating in multiple states across India and works extensively to improve the lives of marginalized communities, including small workers and laborers. They provide vocational training, legal assistance, healthcare services, and educational support to empower workers. Prayas also advocates for labor rights and works towards policy changes for the welfare of these workers. ",
-    category: "NGO",
-    link: "http://www.prayaschildren.org/",
-  },
-  {
-    name: "Association for Rural and Urban Needy (ARUN)",
-    image:
-      "https://res.cloudinary.com/dw2dqblhs/image/upload/v1684300687/Janseva/El8q1-dVgAABLj5_ayt0ga.png",
-    description:
-      "ARUN is an NGO that focuses on socio-economic development and upliftment of disadvantaged communities in rural and urban areas. They support small workers, laborers, electricians, and plumbers by offering skill development programs, microfinance opportunities, and access to essential services. ARUN also helps in creating sustainable livelihoods for these workers through entrepreneurship development and capacity building",
-    category: "NGO",
-    link: "http://arunindia.org/",
-  },
-  {
-    name: "Smile Foundation",
-    image:
-      "https://res.cloudinary.com/dw2dqblhs/image/upload/v1684300755/Janseva/sm-e_TTy_400x400_vegcdn.jpg",
-    description:
-      "Smile Foundation is a national-level development organization in India that works towards the education, healthcare, and livelihood support of underprivileged communities. They have initiatives specifically targeting small workers and laborers. The organization provides skill training programs, healthcare services, education support, and livelihood enhancement opportunities. Smile Foundation also collaborates with various stakeholders to improve policy frameworks and ensure better working conditions for these workers. ",
-    category: "NGO",
-    link: "https://www.smilefoundationindia.org/",
-  },
+  
 ];
 
 module.exports = {SeedData}
